15043fe376366f27313e83aab6a6444f050451bc
Jacob Hunt <jacobhuntemail@gmail.com>
2023-05-15 19:31:16 -0400

    Navbar styling and labels

--- portfolio-site/src/App.tsx
+++ portfolio-site/src/App.tsx
@@ -9,13 +9,15 @@ function App() {
                 <Navbar
                     links={[
                         { Text: "Jacob Hunt", Href: "#pg1" },
-                        { Text: "Test Link 2", Href: "#pg2" },
-                        { Text: "Test Link 3", Href: "#pg3" },
+                        { Text: "About", Href: "#pg2" },
+                        { Text: "Portfolio", Href: "#pg3" },
+                        { Text: "Contact", Href: "#pg4" }
                     ]}
                 />
                 <div id="pg1" style={{ height: "100vh", backgroundColor: "green" }}></div>
                 <div id="pg2" style={{ height: "100vh", backgroundColor: "blue" }}></div>
                 <div id="pg3" style={{ height: "100vh", backgroundColor: "red" }}></div>
+                <div id="pg4" style={{ height: "100vh", backgroundColor: "yellow" }}></div>
             </header>
         </div>
     );
