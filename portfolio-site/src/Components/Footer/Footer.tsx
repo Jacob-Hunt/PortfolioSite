86ce8d4db07bfa9fe1931bfb8a62d3b47b044e56
Jacob Hunt <jacobhuntemail@gmail.com>
2023-05-21 17:48:59 -0400

    Patch link vulnerability in footer

--- portfolio-site/src/Components/Footer/Footer.tsx
+++ portfolio-site/src/Components/Footer/Footer.tsx
@@ -17,7 +17,7 @@ export default class Footer extends React.Component<
         return (
             <div className={ styles.footer }>
                 <p>
-                    This website was designed and built by <a href="http://github.com/jacob-hunt/" target="_blank">Jacob Hunt</a>. | Powered by <a href="https://react.dev/" target="_blank">React</a>, <a href="https://nodejs.org/en" target="_blank">Node</a>, and <a href="https://www.typescriptlang.org/" target="_blank">TypeScript</a>. | Hosted by <a href="https://render.com/" target="_blank">Render</a>
+                    This website was designed and built by <a href="http://github.com/jacob-hunt/" target="_blank" rel="noreferrer">Jacob Hunt</a>. | Powered by <a href="https://react.dev/" target="_blank" rel="noreferrer">React</a>, <a href="https://nodejs.org/en" target="_blank" rel="noreferrer">Node</a>, and <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">TypeScript</a>. | Hosted by <a href="https://render.com/" target="_blank" rel="noreferrer">Render</a>
                 </p>
             </div>
         )
