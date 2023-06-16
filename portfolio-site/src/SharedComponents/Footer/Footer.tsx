2f8be956171c8e11c3ff39c7204fd3ef016972cc
Jacob Hunt <jacobhuntdevelopment@gmail.com>
2023-06-15 23:06:44 -0400

    Update footer link

--- portfolio-site/src/SharedComponents/Footer/Footer.tsx
+++ portfolio-site/src/SharedComponents/Footer/Footer.tsx
@@ -17,7 +17,7 @@ export default class Footer extends React.Component<
         return (
             <div className={ styles.footer }>
                 <p>
-                    This website was designed and built by <a href="http://github.com/jacob-hunt/" target="_blank" rel="noreferrer">Jacob Hunt</a>. | Powered by <a href="https://react.dev/" target="_blank" rel="noreferrer">React</a>, <a href="https://nodejs.org/en" target="_blank" rel="noreferrer">Node</a>, and <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">TypeScript</a>. | Hosted by <a href="https://render.com/" target="_blank" rel="noreferrer">Render</a>
+                    This website was designed and built by <a href="https://www.linkedin.com/in/jhweb/" target="_blank" rel="noreferrer">Jacob Hunt</a>. | Powered by <a href="https://react.dev/" target="_blank" rel="noreferrer">React</a>, <a href="https://nodejs.org/en" target="_blank" rel="noreferrer">Node</a>, and <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">TypeScript</a>. | Hosted by <a href="https://render.com/" target="_blank" rel="noreferrer">Render</a>
                 </p>
             </div>
         )
